c03f0f6b50d7fc76dad24f55304cdbb34e6646f8
Ben Doyle <doyle.ben@gmail.com>
2011-05-09 19:39:56 -0400

    extract prime factors logic

---
+++ common.js
@@ -0,0 +1,30 @@
+function factor(number, factorAttempt, factors) {
+  stopAt         = Math.sqrt(number);
+  currentNumber  = number;
+  currentFactors = factors;
+  while( factorAttempt <= stopAt ) {
+    if( currentNumber / factorAttempt == Math.floor(currentNumber / factorAttempt) ) {
+      currentNumber  = currentNumber / factorAttempt
+      currentFactors.push(factorAttempt);
+      currentFactors = factor( currentNumber, factorAttempt, currentFactors);
+    }
+    else {
+      if( factorAttempt == 2 ){
+        factorAttempt += 1
+      }
+      else {
+        factorAttempt += 2
+      }
+    }
+  }
+  if( currentNumber == number ) {
+    currentFactors.push(currentNumber)
+  }
+  return currentFactors;
+}
+
+function primeFactors(number) {
+  return factor(number,2,[]);
+}
+
+exports.primeFactors = primeFactors;
--- problem3.js
+++ problem3.js
@@ -1,31 +1,4 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
-
-num = 600851475143;
-
-function factor(number, factorAttempt, factors) {
-  stopAt         = Math.sqrt(number);
-  currentNumber  = number;
-  currentFactors = factors;
-  while( factorAttempt <= stopAt ) {
-    if( currentNumber / factorAttempt == Math.floor(currentNumber / factorAttempt) ) {
-      currentNumber  = currentNumber / factorAttempt
-      currentFactors.push(factorAttempt);
-      currentFactors = factor( currentNumber, factorAttempt, currentFactors);
-    }
-    else {
-      if( factorAttempt == 2 ){
-        factorAttempt += 1
-      }
-      else {
-        factorAttempt += 2
-      }
-    }
-  }
-  if( currentNumber == number ) {
-    currentFactors.push(currentNumber)
-  }
-  return currentFactors;
-}
-
-console.log(factor(num,2,[]))
+var com = require("./common");
+console.log(com.primeFactors(600851475143));
