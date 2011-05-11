4096b5cbe12c0e7a94513011b0cd42604d14d0c4
Ben Doyle <doyle.ben@gmail.com>
2011-05-10 22:29:10 -0400

    problem 6

---
+++ problem6.js
@@ -0,0 +1,18 @@
+// The sum of the squares of the first ten natural numbers is,
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+var com = require("./common");
+
+sum = com.series(100);
+sqOfSum = sum*sum;
+
+sumOfSq = 0
+for(i=1;i<=100;i++){
+  sumOfSq += i*i;  
+}
+
+console.log(sqOfSum - sumOfSq)
